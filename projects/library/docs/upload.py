13be0315c6981b8431b83ec21c0fd5517d8b8c3b
liz-antora <liz.theurer@antora.energy>
2025-06-03 11:35:28 +0545

    wip

---
+++ projects/library/docs/upload.py
@@ -0,0 +1,67 @@
+import argparse
+import json
+import os
+from pathlib import Path
+
+from notion_client import Client
+
+notion = Client(auth=os.environ["NOTION_TOKEN"])  # integration secret
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description="Upload to Notion")
+    # parser.add_argument(
+    #     "-f", "--file", help="JSON File to upload", required=True, type=Path
+    # )
+    # parser.add_argument(
+    #     "-p",
+    #     "--parent_page_id",
+    #     help="Parent page ID (integration connected)",
+    #     required=True,
+    # )
+    # parser.add_argument(
+    #     "-t", "--title", help="Title of the new page", required=True
+    # )
+    # args = parser.parse_args()
+
+    # with args.file.open("r", encoding="utf-8") as f:
+    #     contents = json.load(f)
+
+    # new_page = notion.pages.create(
+    #     parent={"type": "page_id", "page_id": args.parent_page_id},
+    #     properties={
+    #         "title": {"title": [{"text": {"content": args.title}}]},
+    #     },
+    #     children=contents,
+    # )
+
+    p = Path(__file__).parents[0]/Path('build/notion/index.json')
+    with p.open("r", encoding="utf-8") as f:
+        contents = json.load(f)
+    print(contents)
+
+    # contents = [
+    #     {
+    #         "object": "block",
+    #         "type": "paragraph",
+    #         "paragraph": {
+    #             "rich_text": [
+    #                 {
+    #                     "type": "text",
+    #                     "text": {
+    #                         "content": (
+    #                             "The content of your page goes here. See https://developers.notion.com/reference/post-page"
+    #                         )
+    #                     },
+    #                 }
+    #             ]
+    #         },
+    #     }
+    # ]
+    new_page = notion.pages.create(
+        parent={"type": "page_id", "page_id": '2068a6614c28801dacaaf2ea81d9743d'},
+        properties={
+            "title": {"title": [{"text": {"content": 'Docs'}}]},
+        },
+        children=contents,
+    )
+    print(new_page)
--- projects/library/library/main.py
+++ projects/library/library/main.py
@@ -2,5 +2,6 @@ from loguru import logger as log
 
 
 def hello(name: str) -> str:
+    '''This is a wonderful hello function!'''
     log.debug(name)
     return f"Hello from {name}!"
